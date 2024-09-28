64cbbabe2795c02a516550acf267c962fc80844b
Neco_Stack <necoabay@googlemail.com>
2024-09-28 14:46:48 +0200

    operators

---
+++ src/Intro-TS-Level-1_13/basic.ts
@@ -0,0 +1,20 @@
+
+// Addition
+let addition_operator: number = 1 + 1; 
+console.log('addition: ' + addition_operator);
+// Subtraktion
+let subtraktion_operator: number = 2 - 1;
+console.log('subtraktion: ' + subtraktion_operator);
+// Multiplikation
+let multiplication_operator: number = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+// Division
+let division_operator: number = 4 / 2; 
+console.log('division: ' + division_operator);
+// Modulu: 5 passt 2x in 14 und somit bleibt ein Rest von 4
+let modulo_operator: number = 14 % 5;
+console.log('modulo: ' + modulo_operator);
+
+
+
+
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="./src/Intro-TS-Level-1_13/basic.html">Intro-TS-Level-1_13</a>
+  </div>
+`
+
+
